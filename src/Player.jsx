5b3a9ff299b696a5327711be5a8b67905b79b7b4
Jose "JJ" Ortiz <jortiz00@gmail.com>
2023-07-15 22:00:46 -0700

    Add changes as directed in the exercises

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,31 +1,45 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+import { useState } from 'react';
 function Player(prop) {
-  function addPoint (){
+	const [score, setPoints,] = useState(0);
 
-  }
-  return (
-    <div className="container-fluid border border-dark p-3 m-3">
-      <div className="row justify-content-center">
-        {/* Render Name here */}
-        <h2 key={prop.id} className="text-center">{prop.name}</h2>
-      </div>
-      <div className="row justify-content-center">
-        <p className="text-center fs-3">{prop.score}</p>
-      </div>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          {/* Add increment event handler */}
-          <button className="btn btn-outline-primary w-100">Add Point +</button>
-        </div>
-        <div className="col-md-6">
-          {/* Add decrement event handler */}
-          <button className="btn btn-outline-danger w-100">
-            Remove Point -
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+
+	function addPoints() {
+		setPoints(score + 1);
+	}
+
+	function takePoints() {
+    if(score > 0){
+		setPoints(score - 1);
+    }}
+
+	return (
+		<div className='container-fluid border border-dark p-3 m-3'>
+			<div className='row justify-content-center'>
+				{/* Render Name here */}
+				<h2 key={prop.id} className='text-center'>
+					{prop.name}
+				</h2>
+			</div>
+			<div className='row justify-content-center'>
+				<p className='text-center fs-3'>{score}</p>
+			</div>
+			<div className='row justify-content-center'>
+				<div className='col-md-6'>
+					{/* Add increment event handler */}
+					<button onClick={addPoints} className='btn btn-outline-primary w-100'>
+						Add Point +
+					</button>
+				</div>
+				<div className='col-md-6'>
+					{/* Add decrement event handler */}
+					<button onClick={takePoints} className='btn btn-outline-danger w-100'>
+						Remove Point -
+					</button>
+				</div>
+        <button onClick={prop.removePlayer} className='mt-4 btn btn-warning w-100'>Remove Player</button>
+			</div>
+		</div>
+	);
 }
 export default Player;
--- src/ScoreBoard.jsx
+++ src/ScoreBoard.jsx
@@ -7,20 +7,34 @@ function ScoreBoard() {
 	const playerNameRef = useRef();
 	function addPlayer() {
 		let currentPlayerText = playerNameRef.current.value || undefined;
-
-		setPlayer([
-			...players,
-			{
-				name: currentPlayerText,
-				id: Date.now(),
-			},
-		]);
+		if (currentPlayerText !== undefined) {
+			setPlayer([
+				...players,
+				{
+					name: currentPlayerText,
+					id: Date.now(),
+				},
+			]);
+		} else {
+			alert('Please enter a Player or Team Name');
+		}
 		playerNameRef.current.value = '';
 	}
+
+	function removePlayer(id) {
+		setPlayer(
+			//returns an array that excludes all items with toDo.id = the inputted id
+			players.filter((player) => {
+				return player.id !== id;
+			})
+		);
+	}
+
 	return (
 		<div className='container'>
 			<div className='row  text-center'>
-				<h1>ScoreBoard</h1>
+				<h1>JJ&apos;s Score Board</h1>
+				<span>Track your Team or Player Score here</span>
 			</div>
 			<div className='row'>
 				<div className='col-md-4 m-auto'>
@@ -30,15 +44,15 @@ function ScoreBoard() {
 						<input
 							ref={playerNameRef}
 							type='text'
-							className='form-control'
-							placeholder='New Player Name'
-							aria-label='New Player Name'
-							aria-describedby='addPlayer'
+							className='mt-4 form-control'
+							placeholder='Please enter Player or Team Name'
+							aria-label='New Player Name or Team'
+							aria-describedby='addPlayerOrTeam'
 							required
 						/>
 						{/* add Add Player event handling to this button */}
 						<button
-							className='btn btn-outline-primary'
+							className='mt-4 btn btn-outline-primary'
 							type='button'
 							id='addPlayer'
 							onClick={addPlayer}>
@@ -55,6 +69,8 @@ function ScoreBoard() {
 							<Player
 								key={players.id}
 								name={players.name}
+								score='0'
+								removePlayer={() => removePlayer(players.id)}
 								// Anonymous arrow function that we can hold off activating
 								// until the user clicks a button inside of the Player component
 							/>
